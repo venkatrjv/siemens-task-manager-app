94d6614725ebfbfa2c9fbffcc791042846c6df6f
Rajeev Santhosh <VBetha@its.jnj.com>
2025-08-21 20:48:26 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
-import Login from "./components/Login";
-import Dashboard from "./components/Dashboard";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const App: React.FC = () => {
